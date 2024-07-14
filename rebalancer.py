32082641514c094aad21534db45a19100998cb7f
BastianSetter <bastian.setter@gmail.com>
2024-07-14 19:22:25 +0200

    further refactoring

--- rebalancer.py
+++ rebalancer.py
@@ -17,6 +17,9 @@ class Rebalancer():
             trigger.check_trigger(date, portfolio)
         if not rebalance_today: return 
 
+        self.execute_rebalance(date, portfolio)
+
+    def execute_rebalance(self, date, portfolio):
         #TODO: buy and sell can be combined if enough accuracy in cash terms is acchieved (including fees, taxes)
         # and further preassignment takes place
         self.sell_over_positions(portfolio, date)
@@ -24,7 +27,6 @@ class Rebalancer():
 
     def sell_over_positions(self, portfolio, date:date):
         total_portfolio_value = portfolio.calculate_total_value(date)
-        new_orders = []
         for asset in portfolio.assets:
             if (rebalance_amount := asset.rebalance_amount(total_portfolio_value, date)) > 0:
                 sell_order = asset.sell_amount(rebalance_amount, date)
@@ -36,7 +38,7 @@ class Rebalancer():
     def buy_under_positions(self, portfolio, date:date):
         total_portfolio_value = portfolio.calculate_total_value(date)
         for asset in portfolio.assets:
-            if (rebalance_amount := asset.rebalance_amount(total_portfolio_value, date)) - 0:
+            if (rebalance_amount := asset.rebalance_amount(total_portfolio_value, date)) < 0:
                 buy_order = asset.buy_amount(-rebalance_amount, date)
                 portfolio.combined_order_history.append(buy_order)
                 used_cash = buy_order.amount*buy_order.order_price-(buy_order.fees+buy_order.tax)
